af9ca4b8460e66b3ac430359c367e32ecffeffd0
abdikani <abdikani@gmail.com>
2024-11-18 16:29:01 +0300

    first commit

---
+++ dom.js
@@ -0,0 +1,14 @@
+
+// let head=document.querySelector('.head');
+// head.innerHTML= "hello";
+// head.style.color="red";
+// h2= document.createElement("h2");
+// h2.textcontent="waa h2"
+// head.appendChild(h2);
+ let count=document.querySelector('.count');
+ function tasbiix(){
+    count.textContent =parseInt(count.textContent)+1
+ }
+ function reset(){
+   count.textContent=0
+ }
